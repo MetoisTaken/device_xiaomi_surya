e4f62e3db646a05e2ac2d86dc31db706f8120304
Andrew Thomas <44932193+itz63c@users.noreply.github.com>
2019-03-26 21:44:19 -0400

    surya: Add Notch Bar Killer overlay
     * This overlay gets rid of the black bar in most apps/games
       and lets the screen fill past the notch.
    
    Change-Id: Ic008ce5d569127d230dc44451929477e063c4471
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- device.mk
+++ device.mk
@@ -319,7 +319,8 @@ PRODUCT_PACKAGES += \
     KarnaPowerProfile \
     KarnaSettingsProviderOverlay \
     SuryaPowerProfile \
-    SuryaSettingsProviderOverlay
+    SuryaSettingsProviderOverlay \
+    NotchBarKillerSurya
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
